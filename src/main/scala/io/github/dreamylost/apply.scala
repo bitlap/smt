6562ea5626e653c25b0c8d5f6fe02151467edcdb
梦境迷离 <dreamylost@outlook.com>
2021-07-07 00:53:26 +0800

    refactor code folder

--- src/main/scala/io/github/dreamylost/apply.scala
+++ src/main/scala/io/github/dreamylost/apply.scala
@@ -16,6 +16,5 @@ import scala.annotation.{ StaticAnnotation, compileTimeOnly }
 final class apply(
     verbose: Boolean = false
 ) extends StaticAnnotation {
-  import scala.language.experimental.macros
   def macroTransform(annottees: Any*): Any = macro applyMacro.impl
 }
