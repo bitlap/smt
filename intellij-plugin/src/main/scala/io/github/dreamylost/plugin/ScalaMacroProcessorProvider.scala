ba19faaebbb6e4b3bd0c2f3fe26fb3a079fa0e23
IceMimosa <chk19940609@gmail.com>
2021-07-07 19:05:38 +0800

    Add @json

--- intellij-plugin/src/main/scala/io/github/dreamylost/plugin/ScalaMacroProcessorProvider.scala
+++ intellij-plugin/src/main/scala/io/github/dreamylost/plugin/ScalaMacroProcessorProvider.scala
@@ -2,7 +2,7 @@ package io.github.dreamylost.plugin
 
 import com.intellij.openapi.Disposable
 import io.github.dreamylost.plugin.processor.Processor
-import io.github.dreamylost.plugin.processor.clazz.{ BuilderProcessor, LogProcessor }
+import io.github.dreamylost.plugin.processor.clazz.{ BuilderProcessor, JsonProcessor, LogProcessor }
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.{ ScClass, ScObject, ScTypeDefinition }
 
 import scala.collection.mutable
@@ -17,6 +17,7 @@ class ScalaMacroProcessorProvider extends Disposable {
   private val processors = mutable.Map[String, Processor]()
   regist(ScalaMacroNames.BUILDER, new BuilderProcessor())
   regist(ScalaMacroNames.LOG, new LogProcessor())
+  regist(ScalaMacroNames.JSON, new JsonProcessor())
 
   override def dispose(): Unit = {}
 
@@ -30,7 +31,7 @@ class ScalaMacroProcessorProvider extends Disposable {
   def findProcessors(source: ScTypeDefinition): Seq[Processor] = {
     processors.filter { p =>
       source match {
-        case obj: ScObject  => obj.fakeCompanionClassOrCompanionClass.hasAnnotation(p._1)
+        case obj: ScObject  => obj.hasAnnotation(p._1) || obj.fakeCompanionClassOrCompanionClass.hasAnnotation(p._1)
         case clazz: ScClass => clazz.hasAnnotation(p._1)
         case _              => source.hasAnnotation(p._1)
       }
