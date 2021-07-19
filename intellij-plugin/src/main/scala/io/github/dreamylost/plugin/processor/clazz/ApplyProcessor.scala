cbe534e98b2ae24a74f0399be3d55f5c615d70df
IceMimosa <chk19940609@gmail.com>
2021-07-19 23:28:55 +0800

    Add

--- intellij-plugin/src/main/scala/io/github/dreamylost/plugin/processor/clazz/ApplyProcessor.scala
+++ intellij-plugin/src/main/scala/io/github/dreamylost/plugin/processor/clazz/ApplyProcessor.scala
@@ -22,10 +22,10 @@ class ApplyProcessor extends AbsProcessor {
           case obj: ScObject =>
             val clazz = obj.fakeCompanionClassOrCompanionClass.asInstanceOf[ScClass]
             val nameAndTypes = getConstructorCurryingParameters(clazz, withSecond = false)
-              .map(_.map(o => s"${o._1}: ${o._2}").mkString(", "))
-              .mkString(")(")
+              .map(_.map(o => s"${o._1}: ${o._2}").mkString("(", ", ", ")"))
+              .mkString
             val genericTypes = clazz.typeParamString
-            Seq(s"def apply$genericTypes($nameAndTypes): ${clazz.getName}$genericTypes = ???")
+            Seq(s"def apply$genericTypes$nameAndTypes: ${clazz.getName}$genericTypes = ???")
           case _ => Nil
         }
       case _ => Nil
