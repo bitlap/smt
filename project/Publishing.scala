f5eadf1725607ad31b7e2b0f60188083f01753d8
IceMimosa <chk19940609@gmail.com>
2021-12-08 16:03:03 +0800

    release 0.3.4

--- project/Publishing.scala
+++ project/Publishing.scala
@@ -15,7 +15,7 @@ object Publishing {
   lazy val publishSettings = Seq(
     credentials += Credentials(Path.userHome / ".ivy2" / ".sonatype_credentials"),
     publishTo := {
-      val nexus = "https://oss.sonatype.org/"
+      val nexus = "https://s01.oss.sonatype.org/"
       if (isSnapshot.value)
         Some("snapshots" at nexus + "content/repositories/snapshots")
       else
@@ -31,7 +31,14 @@ object Publishing {
         name = "梦境迷离",
         email = "dreamylost@outlook.com",
         url = url("https://dreamylost.cn")
-      )),
+      ),
+      Developer(
+        id = "IceMimosa",
+        name = "ChenKai",
+        email = "chk19940609@gmail.com",
+        url = url("http://patamon.me")
+      )
+    ),
     sonatypeProfileName := organization.value,
     isSnapshot := version.value endsWith "SNAPSHOT",
     homepage := Some(url("https://bitlap.org")),
