2f4fad54c9b14e5717c863888eeb1602382ccbee
梦境迷离 <dreamylost@outlook.com>
2022-03-20 16:50:57 +0800

    remove plugin from repository

--- cacheable/src/main/scala/org/bitlap/cacheable/ZRedisLive.scala
+++ cacheable/src/main/scala/org/bitlap/cacheable/ZRedisLive.scala
@@ -40,8 +40,8 @@ case class ZRedisLive(private val rs: Redis) extends ZRedisService {
   override def hSet[T: Schema](key: String, field: String, value: T): ZIO[ZRedisCacheService, RedisError, Long] =
     redis.hSet[String, String, T](key, field -> value).provideLayer(redisLayer)
 
-  override def hGet[T](key: String, field: String)(implicit schema: Schema[T]): ZIO[ZRedisCacheService, RedisError, Option[T]] =
-    redis.hGet(key, field).returning[T](schema).provideLayer(redisLayer)
+  override def hGet[T: Schema](key: String, field: String): ZIO[ZRedisCacheService, RedisError, Option[T]] =
+    redis.hGet(key, field).returning[T].provideLayer(redisLayer)
 
   override def hGetAll[T: Schema](key: String): ZIO[ZRedisCacheService, RedisError, Map[String, T]] =
     redis.hGetAll(key).returning[String, T].provideLayer(redisLayer)
