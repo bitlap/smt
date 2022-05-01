dca0ac99ac688c13109e5a37a9941664767c484b
梦境迷离 <dreamylost@outlook.com>
2022-05-01 22:08:55 +0800

    support default

--- csv-core/src/main/scala/org/bitlap/csv/core/ScalableImplicits.scala
+++ csv-core/src/main/scala/org/bitlap/csv/core/ScalableImplicits.scala
@@ -27,35 +27,35 @@ package org.bitlap.csv.core
  */
 trait ScalableImplicits {
 
-  implicit val stringCSVConverter: Scalable[String] = new Scalable[String] {
+  implicit val stringScalable: Scalable[String] = new Scalable[String] {
     override def _toScala(column: String): Option[String] = if (column.isEmpty) None else Some(column)
   }
 
-  implicit val intCsvConverter: Scalable[Int] = new Scalable[Int] {
+  implicit val intScalable: Scalable[Int] = new Scalable[Int] {
     override def _toScala(column: String): Option[Int] = Option(column.toInt)
   }
 
-  implicit val charCsvConverter: Scalable[Char] = new Scalable[Char] {
+  implicit val charScalable: Scalable[Char] = new Scalable[Char] {
     override def _toScala(column: String): Option[Char] = if (column.isEmpty) None else Some(column.charAt(0))
   }
 
-  implicit val longCsvConverter: Scalable[Long] = new Scalable[Long] {
+  implicit val longScalable: Scalable[Long] = new Scalable[Long] {
     override def _toScala(column: String): Option[Long] = Option(column.toLong)
   }
 
-  implicit val shortCsvConverter: Scalable[Short] = new Scalable[Short] {
+  implicit val shortScalable: Scalable[Short] = new Scalable[Short] {
     override def _toScala(column: String): Option[Short] = Option(column.toShort)
   }
 
-  implicit val doubleCsvConverter: Scalable[Double] = new Scalable[Double] {
+  implicit val doubleScalable: Scalable[Double] = new Scalable[Double] {
     override def _toScala(column: String): Option[Double] = Option(column.toDouble)
   }
 
-  implicit val floatCsvConverter: Scalable[Float] = new Scalable[Float] {
+  implicit val floatScalable: Scalable[Float] = new Scalable[Float] {
     override def _toScala(column: String): Option[Float] = Option(column.toFloat)
   }
 
-  implicit val booleanCsvConverter: Scalable[Boolean] = new Scalable[Boolean] {
+  implicit val booleanScalable: Scalable[Boolean] = new Scalable[Boolean] {
     override def _toScala(column: String): Option[Boolean] = Option(column.toBoolean)
   }
 }
