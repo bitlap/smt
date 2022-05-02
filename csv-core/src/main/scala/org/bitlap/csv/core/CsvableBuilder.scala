7a6de9cd79e2d6ccb99aebdb17fe346fb16e65b4
梦境迷离 <dreamylost@outlook.com>
2022-05-02 19:10:12 +0800

    fix private package

--- csv-core/src/main/scala/org/bitlap/csv/core/CsvableBuilder.scala
+++ csv-core/src/main/scala/org/bitlap/csv/core/CsvableBuilder.scala
@@ -29,7 +29,7 @@ import org.bitlap.csv.core.macros.DeriveCsvableBuilder
  * @author 梦境迷离
  * @version 1.0,2022/4/30
  */
-private[core] class CsvableBuilder[T] {
+final class CsvableBuilder[T] {
 
   /**
    * Convert this CSV column string to any Scala types.
