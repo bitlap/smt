e0d7ddaf21435818b3a7f0b7219675ee2890c455
梦境迷离 <dreamylost@outlook.com>
2023-08-14 12:48:47 +0800

    fix ci
    Signed-off-by: 梦境迷离 <dreamylost@outlook.com>

--- project/Commands.scala
+++ project/Commands.scala
@@ -9,17 +9,12 @@ object Commands {
 
   val FmtSbtCommand = Command.command("fmt")(state => "scalafmtSbt" :: "scalafmtAll" :: state)
 
-  val FixSbtCommand = Command.command("fix")(state => "scalafixEnable" :: "scalafixAll RemoveUnused" :: state)
-
   val FmtSbtCheckCommand =
-    Command.command("check")(state =>
-      "scalafixEnable" :: "scalafixAll --check" :: "scalafmtSbtCheck" :: "scalafmtCheckAll" :: state
-    )
+    Command.command("check")(state => "scalafmtSbtCheck" :: "scalafmtCheckAll" :: state)
 
   val value = Seq(
     FmtSbtCommand,
-    FmtSbtCheckCommand,
-    FixSbtCommand
+    FmtSbtCheckCommand
   )
 
 }
